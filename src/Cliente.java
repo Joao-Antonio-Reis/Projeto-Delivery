0a7ea6b2ebb149bd2aff762a83571631c5e84e84
João Antônio Coelho dos Reis <joaocoelhoreis@gmail.com>
2024-04-17 09:50:09 -0300

    Update Cliente.java

---
+++ src/Cliente.java
@@ -0,0 +1,16 @@
+public class Cliente {
+    
+    private Endereço endereço;
+
+    public Cliente(Endereço endereço) {
+        this.endereço = endereço;
+    }
+
+    public Endereço getEndereço() {
+        return endereço;
+    }
+
+    public void setEndereço(Endereço endereço) {
+        this.endereço = endereço;
+    }
+}
