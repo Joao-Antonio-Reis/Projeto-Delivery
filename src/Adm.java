fa20efc189bb36de87887cb3fb9323ed2d1d5a5d
João Antônio Coelho dos Reis <joaocoelhoreis@gmail.com>
2024-04-17 09:50:24 -0300

    Update Adm.java

---
+++ src/Adm.java
@@ -0,0 +1,38 @@
+public class Adm {
+    
+    private int id;
+    private String login;
+    private String senha;
+    
+    public Adm(int id, String login, String senha) {
+        this.id = id;
+        this.login = login;
+        this.senha = senha;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    
+}
