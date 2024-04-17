f38028530735e2dab17874bd71d01c33655f953b
João Antônio Coelho dos Reis <joaocoelhoreis@gmail.com>
2024-04-17 09:58:54 -0300

    Update Cliente.java

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,15 +1,30 @@
 public class Cliente {
     
+    private String nome;
+    private String telefone;
     private Endereço endereço;
 
-    public Cliente(Endereço endereço) {
+    public Cliente(String nome, String telefone, Endereço endereço) {
+        this.nome = nome;
+        this.telefone = telefone;
         this.endereço = endereço;
     }
 
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
     public Endereço getEndereço() {
         return endereço;
     }
-
     public void setEndereço(Endereço endereço) {
         this.endereço = endereço;
     }
