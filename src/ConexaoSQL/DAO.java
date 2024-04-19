178567c18f0d4f7dcb9d421fe9fc7e458a2b2a01
Jorge Afonso <128873434+MOR4Xx@users.noreply.github.com>
2024-04-19 10:36:03 -0300

    Banco de dados

---
+++ src/ConexaoSQL/DAO.java
@@ -0,0 +1,22 @@
+package ConexaoSQL;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import javax.swing.JOptionPane;
+public class DAO {
+
+    public static void main(String[] args) {
+        final String URL = "jdbc:mysql://localhost:3306/mysql";
+        final String USER = "root";
+        final String SENHA = "";
+        try {
+            Connection connection = DriverManager.getConnection(URL, USER,SENHA);
+            JOptionPane.showMessageDialog(null,"Conexão realizada com sucesso!");
+            connection.close();
+        }catch(SQLException erro) {
+            JOptionPane.showMessageDialog(null,"Problemas na conexão com a fonte de dados \n"+ erro.toString());
+        }
+    }
+}
--- src/Models/Cardapio.java
+++ src/Models/Cardapio.java
@@ -11,37 +11,54 @@ import java.awt.event.ActionListener;
 import javax.swing.*;
 
 
-public class Cardapio extends JLabel{
+public class Cardapio extends JFrame{
+    private JFrame frame;
     private Font font = new Font("Arial", Font.BOLD, 15);
     private Color cor = new Color(136,0, 12);
 
-
-//    public static void main(String[] args) {
-//        EventQueue.invokeLater(() -> {
-//            try {
-//                Cardapio window = new Cardapio();
-//                window.setVisible(true);
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//        });
-//    }
-
-    public Cardapio() {
-        cardapioLabel();
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            try {
+                Cardapio window = new Cardapio();
+                window.setVisible(true);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
     }
 
-    public void cardapioLabel() {
+    public Cardapio() {
+        frame();
+    }
 
+    private void frame() {
+
+        setTitle("Japinha Oriental Food - Cardapio");
         setVisible(true); //Visibilidade true
+        setSize(415,800); //Define o tamanho da tela
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//Fecha o programa quando fecha a janela
+        setResizable(false);//Não deixa mudar o tamanho
+        setLocationRelativeTo(null); //abre o frame no meio da tela
         setLayout(null);//Deixa o layout null
 
-        JLabel cardapio = new JLabel("Cardapio");
-        cardapio.setBounds(5, 5, 100,20);
-        add(cardapio);
+        JMenuBar menuBar = new JMenuBar();
+        // Cria os menus
+        JMenu categoriasMenu = new JMenu("Categoria");
+        JMenu carrinhoMenu = new JMenu("Carrinho");
+        JMenu loginMenu = new JMenu("Login");
 
+        menuBar.setBackground(cor);
+        categoriasMenu.setForeground(Color.WHITE);
+        carrinhoMenu.setForeground(Color.WHITE);
+        loginMenu.setForeground(Color.WHITE);
 
-        JButton submitButton = new JButton("Ta no cardapio");
+        menuBar.add(categoriasMenu);
+        menuBar.add(carrinhoMenu);
+        menuBar.add(loginMenu);
+
+        setJMenuBar(menuBar);
+
+        JButton submitButton = new JButton("Finalizar");
         submitButton.setSize(new Dimension(25,20));
         submitButton.setBounds(270, 695, 125, 40);
         submitButton.setFont(font);
