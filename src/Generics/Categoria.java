0dad9378747171e70332187ec7c625c1c3d8a65d
João Antônio Coelho dos Reis <joaocoelhoreis@gmail.com>
2024-07-04 10:16:00 -0300

    Add files via upload
    10:15

--- src/Generics/Categoria.java
+++ src/Generics/Categoria.java
@@ -1,5 +1,6 @@
 package Generics;
 
+import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -85,7 +86,42 @@ public class Categoria {
             e.printStackTrace();
             System.out.println("Erro ao carregar categorias!");
         }
+        int quantidadeCategoria = listaDeCategoria.size();
         return listaDeCategoria;
+    }
+
+    public static void consultarProdutosPorCategoria(String categoriaNome) {
+
+        // SQL para selecionar produtos com base no nome da categoria
+        String sql = "SELECT produto_nome, produto_categoria, produto_valor FROM produto WHERE produto_categoria = ?";
+
+        try (
+            // Estabelece a conexão com o banco de dados
+            Connection connection = DriverManager.getConnection(URL, USUARIO, SENHA);
+            
+            // Prepara a declaração SQL com um parâmetro
+            PreparedStatement statement = connection.prepareStatement(sql)
+        ) {
+            // Define o valor do parâmetro na consulta SQL
+            statement.setString(1, categoriaNome);
+
+            // Executa a consulta e obtém o resultado
+            ResultSet resultSet = statement.executeQuery();
+
+            // Itera sobre os resultados da consulta
+            while (resultSet.next()) {
+                String nome = resultSet.getString("produto_nome");
+                String categoria = resultSet.getString("produto_categoria");
+                BigDecimal valor = resultSet.getBigDecimal("produto_valor");
+
+                // Exibe os dados dos produtos
+                System.out.println("Nome: " + nome + ", Categoria: " + categoria + " Valor: "+ valor);
+            }
+            
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Erro ao consultar produtos por categoria!");
+        }
     }
 }
 
