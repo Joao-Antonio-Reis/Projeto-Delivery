30ba7852a39e55311e42a46ef3a3b94dd5cf480d
Jorge Afonso <128873434+MOR4Xx@users.noreply.github.com>
2024-07-30 22:40:21 -0300

    Commit 30/07 22:40

--- src/View/CadastroProduto.java
+++ src/View/CadastroProduto.java
@@ -4,19 +4,12 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
-import java.io.IOException;
 import java.math.BigDecimal;
-import java.nio.file.Files;
-import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
-
+import java.util.List;
 import javax.swing.*;
 import javax.swing.border.MatteBorder;
-import javax.swing.event.AncestorEvent;
-import javax.swing.event.AncestorListener;
 import javax.swing.plaf.basic.BasicComboBoxUI;
-import java.math.BigDecimal;
-import java.util.List;
 
 import ConexaoDB.CategoriaDAO;
 import ConexaoDB.ProdutoDAO;
@@ -26,41 +19,29 @@ import Models.Produto;
 
 public class CadastroProduto extends JLabel {
 
-    public JComboBox<String> getCategoriaBox;
     private JFrame frame;
     private JTextField nomeField;
     private JTextArea descProdutoArea;
     private JTextField precoField;
-    private JComboBox categoriaBox;
+    private JComboBox<String> categoriaBox;
     private JLabel imageLabel;
     private JButton removerProduto;
     private JLabel labelProduto;
     private File selectedFile;
     private JButton cadastrarButton;
+    private ControllerProduto controllerProduto = new ControllerProduto();
     private Font font = new Font("Arial", Font.BOLD, 15);
     private Color cor = new Color(136, 0, 12);
-
-
-//    public static void main(String[] args) {
-//        EventQueue.invokeLater(() -> {
-//            try {
-//                CadastroProduto window = new CadastroProduto();
-//                window.frame.setVisible(true);
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//        });
-//    }
+    private JPanel panelRemover;
 
     public CadastroProduto() {
+        setSize(415, 800); // Define o tamanho da tela
+        setLayout(null);
         produtoLabel();
     }
 
-    public void produtoLabel() {
-        ControllerProduto controllerProduto = new ControllerProduto();
+    private void produtoLabel() {
 
-        setSize(415, 800); //Define o tamanho da tela
-        setLayout(null);
 
         JLabel nomeProdLabel = new JLabel("Nome: ");
         nomeProdLabel.setFont(font);
@@ -82,10 +63,10 @@ public class CadastroProduto extends JLabel {
 
         JLabel categoriaLabel = new JLabel("Categoria: ");
         categoriaLabel.setFont(font);
-        categoriaLabel.setBounds(5,55, 90, 20);
+        categoriaLabel.setBounds(5, 55, 90, 20);
         add(categoriaLabel);
         categoriaBox = new JComboBox<>();
-        categoriaBox.setBounds(80,55,150,20);
+        categoriaBox.setBounds(80, 55, 150, 20);
         categoriaBox.setUI(new BasicComboBoxUI() {
             @Override
             protected void installDefaults() {
@@ -93,9 +74,7 @@ public class CadastroProduto extends JLabel {
                 categoriaBox.setBorder(BorderFactory.createEmptyBorder());
                 categoriaBox.setBorder(new MatteBorder(0, 0, 2, 0, cor));
             }
-    
         });
-        controllerProduto.carregaCategoria();
         categoriaBox.setFont(font);
         add(categoriaBox);
 
@@ -129,7 +108,7 @@ public class CadastroProduto extends JLabel {
         descProdutoArea = new JTextArea();
         descProdutoArea.setBounds(5, 120, 390, 100);
         descProdutoArea.setFont(font);
-        descProdutoArea.setAlignmentY(JTextField.TOP); //Ajustar no topo
+        descProdutoArea.setAlignmentY(JTextField.TOP); // Ajustar no topo
         descProdutoArea.setLineWrap(true); // Quebra de linha automática
         descProdutoArea.setWrapStyleWord(true); // Quebra de palavra
         descProdutoArea.setBorder(BorderFactory.createLineBorder(cor, 2));
@@ -140,56 +119,15 @@ public class CadastroProduto extends JLabel {
         removerLabel.setFont(font);
         add(removerLabel);
 
-        JPanel panelRemover = new JPanel();
+        panelRemover = new JPanel();
         panelRemover.setLayout(new BoxLayout(panelRemover, BoxLayout.Y_AXIS)); // Layout para organizar componentes verticalmente
         panelRemover.setSize(400, 200);
-
-        // Obtenção da lista de produtos ordenados
-        ProdutoDAO produtoList = new ProdutoDAO();
-        List<Produto> produtos = produtoList.obterProdutosOrdenados();
-
-
-        for (Produto produto : produtos) {
-            JPanel productPanel = new JPanel();
-            productPanel.setLayout(null);
-            productPanel.setPreferredSize(new Dimension(380, 30)); // Tamanho do retângulo para cada produto
-            //productPanel.setBorder(BorderFactory.createLineBorder(Color.black, 1)); // Borda preta ao redor do painel
-
-
-            // Criação dos componentes para cada produto
-            labelProduto = new JLabel(produto.getNome());
-            labelProduto.setBounds(5, 5 ,200, 20);
-            labelProduto.setFont(new Font("Arial", Font.BOLD, 14));
-            productPanel.add(labelProduto);
-
-            removerProduto = new JButton("Remover");
-            removerProduto.setBackground(cor);
-            removerProduto.setForeground(Color.WHITE);
-            removerProduto.setBounds(250, 5, 100, 25);
-            removerProduto.setFocusPainted(false);
-            removerProduto.setRolloverEnabled(false);
-
-            productPanel.add(removerProduto);
-
-            // Adiciona o painel do produto ao painel do cardápio
-            panelRemover.add(productPanel);
-
-            removerProduto.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    ProdutoDAO produtoDAO = new ProdutoDAO();
-                    produtoDAO.removerProduto(produto.getNome());
-                    productPanel.setVisible(false);
-                }
-            });
-        }
 
         // Criação do painel de rolagem para o remover produto
         JScrollPane scrollPane = new JScrollPane(panelRemover);
         scrollPane.setBounds(0, 265, 400, 300); // Define a posição e tamanho do painel de rolagem
         scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS); // Sempre mostra a barra de rolagem vertical
         scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER); // Nunca mostra a barra de rolagem horizontal
-        // scrollPane.setBorder(BorderFactory.createLineBorder(Color.black, 2)); // Borda preta ao redor do painel de rolagem
         add(scrollPane);
 
         cadastrarButton = new JButton("Cadastrar");
@@ -198,7 +136,6 @@ public class CadastroProduto extends JLabel {
         cadastrarButton.setForeground(Color.WHITE);
         cadastrarButton.setBackground(cor);
         add(cadastrarButton);
-
     }
 
     public String getNome() {
@@ -221,6 +158,10 @@ public class CadastroProduto extends JLabel {
         return (selectedFile != null) ? selectedFile.getName() : "";
     }
 
+    public File getSelectedFile() {
+        return selectedFile;
+    }
+
     public JButton getCadastrarButton() {
         return cadastrarButton;
     }
@@ -229,10 +170,33 @@ public class CadastroProduto extends JLabel {
         categoriaBox.addItem(categoria);
     }
 
-    public void limparCampos(){
+    public void limparCampos() {
         nomeField.setText("");
         precoField.setText("");
         descProdutoArea.setText("");
+    }
 
+    public void adicionarProdutoParaRemover(Produto produto, ActionListener removerAction) {
+        JPanel productPanel = new JPanel();
+        productPanel.setLayout(null);
+        productPanel.setPreferredSize(new Dimension(380, 30)); // Tamanho do retângulo para cada produto
+
+        labelProduto = new JLabel(produto.getNome());
+        labelProduto.setBounds(5, 5, 200, 20);
+        labelProduto.setFont(new Font("Arial", Font.BOLD, 14));
+        productPanel.add(labelProduto);
+
+        removerProduto = new JButton("Remover");
+        removerProduto.setBackground(cor);
+        removerProduto.setForeground(Color.WHITE);
+        removerProduto.setBounds(250, 5, 100, 25);
+        removerProduto.setFocusPainted(false);
+        removerProduto.setRolloverEnabled(false);
+        removerProduto.addActionListener(removerAction);
+
+        productPanel.add(removerProduto);
+        panelRemover.add(productPanel);
+        panelRemover.revalidate();
+        panelRemover.repaint();
     }
 }
