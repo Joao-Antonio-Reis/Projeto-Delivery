2934ae1851ddb9fa078ca70d08d9dc2e853c110d
Jorge Afonso <128873434+MOR4Xx@users.noreply.github.com>
2024-07-31 16:14:53 -0300

    Delete Controllers directory

--- Controllers/Main.java
+++
@@ -1,9 +0,0 @@
-package Controllers;
-
-import View.MenuPrincipalView;
-
-public class Main {
-    public static void main(String[] args) {
-        ControllerMenuPrincipal menuPrincipal = new ControllerMenuPrincipal(new MenuPrincipalView());
-    }
-}
